b35090355e9236f458a7b7f3796ae344146b089f
blakehayama <blake.hayama@lhsla.org>
2026-08-31 09:02:30 -0700

    assignment 1a is done

---
+++ assignment 1a.py
@@ -0,0 +1,16 @@
+# Hayama, Blake
+# Computer Programming p4
+# Assignment 1a
+# August 27 2026
+
+print ("Hello World!")
+print ("Hello my name is Blake!")
+print ("I love python!")
+print ("16 years old")
+print (5+5)
+print (4*3)
+print (50 - 40)
+print (100/5) (100//5) #one is integer division and the other is in float division
+print (2**4)
+print (6*3-4+8//2)
+
